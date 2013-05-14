8f8688e2dbdc243419882b516de53d5266876a5f
kaymmm <kmiyake@gc.cuny.edu>
2013-05-14 15:10:11 -0400

    post meta comments and tags fixes

--- postmeta.php
+++ postmeta.php
@@ -1,8 +1,9 @@
 	<aside class="entry-meta muted">
 		<p><i class="icon-calendar"></i> <time datetime="<?php the_time(get_option('date_format')); ?>" pubdate><?php the_time(get_option('date_format')); ?></time></p>
 		<?php if ( comments_open() ) : ?>
-		<p><i class="icon-comment"></i> <?php comments_popup_link( __("No Comments","bonestheme"), __( "1 Comment", "bonestheme"), __( "% Comments", "bonestheme" ) ); ?></p>
+		<p><i class="icon-comment"></i> <?php comments_popup_link( __("Leave a comment","bonestheme"), __( "One Comment", "bonestheme"), __( "% Comments", "bonestheme" ) ); ?></p>
 		<?php endif; // comments_open() ?>
+		<?php if ( ! is_page() ) : ?>
 		<p><i class="icon-folder-close"></i> <span class="muted"> <?php the_category(", "); ?></span></p>
 		<p><i class="icon-tags"></i>
 		<?php
@@ -13,9 +14,9 @@
 				<a href="<?php echo $tag_link; ?>" title="<?php echo $tag->name; ?> Tag" class="label"><?php echo $tag->name; ?></a>
 			<?php endforeach;
 		else :
-			__("No Tags","bonestheme");
-			echo "No Tags";
+			_e("Not Tagged","bonestheme");
 		endif; ?>
 		</p>
+		<?php endif; ?>
 		<?php edit_post_link( __( 'Edit', 'bonestheme' ), '<p class="edit-link">', '</p>' ); ?>
 	</aside>
