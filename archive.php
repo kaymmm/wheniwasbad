f722ac7747eebc06f2e0a888726c0040e14c9aed
kaymmm <kmiyake@gc.cuny.edu>
2013-05-16 15:05:14 -0400

    Revert "update page_navi calls to bones_page_navi (re:bones update)"
    This reverts commit 38bb30eb458aa3d03e3b3c15364263be7a9e695c.

--- archive.php
+++ archive.php
@@ -46,9 +46,9 @@
 
 					<?php endwhile; // end of the loop. ?>
 					
-					<?php if (function_exists('bones_page_navi')) : // if expirimental feature is active ?>
+					<?php if (function_exists('page_navi')) : // if expirimental feature is active ?>
 						
-						<?php bones_page_navi(); // use the page navi function ?>
+						<?php page_navi(); // use the page navi function ?>
 
 					<?php else : // if it is disabled, display regular wp prev & next links ?>
 						<nav class="wp-prev-next">
