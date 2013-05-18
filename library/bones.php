3d2625f948352d24c51fd5b9220ea23149f96281
kaymmm <kmiyake@gc.cuny.edu>
2013-05-18 03:37:01 -0400

    remove stale function reference

--- library/bones.php
+++ library/bones.php
@@ -166,7 +166,7 @@ function bones_theme_support() {
 	// adding sidebars to Wordpress (these are created in functions.php)
 	add_action( 'widgets_init', 'bones_register_sidebars' );
 	// adding the bones search form (created in functions.php)
-	add_filter( 'get_search_form', 'bones_wpsearch' );
+//	add_filter( 'get_search_form', 'bones_wpsearch' );
 	
 
  
