2dcf7d23b8895b6128cc1fbb3e23b4cd1ffb0cd1
kaymmm <kmiyake@gc.cuny.edu>
2013-05-23 14:27:39 -0400

    Add "more" to excerpts

--- functions.php
+++ functions.php
@@ -157,6 +157,12 @@ function bones_register_sidebars() {
     */
 } // don't remove this bracket!
 
+/************* Excerpts *********************/
+function new_excerpt_more( $more ) {
+	return ' [&hellip;]<br /><a class="read-more btn pull-right" href="'. get_permalink( get_the_ID() ) . '">Read More</a>';
+}
+add_filter( 'excerpt_more', 'new_excerpt_more' );
+
 /************* COMMENT LAYOUT *********************/
 		
 // Comment Layout
