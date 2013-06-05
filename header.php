770e0c393c5dd8ccfe7ce3c3444ed52f4ad2e8f8
kaymmm <kmiyake@gc.cuny.edu>
2013-06-05 01:53:46 -0400

    remove google webfont loader, remove adding theme options from header.php

--- header.php
+++ header.php
@@ -33,61 +33,6 @@
 		<!-- or, set /favicon.ico for IE10 win -->
 		<meta name="msapplication-TileColor" content="#f01d4f">
 		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
-		
-		<!-- Google/Typekit Webfont Loader Scripts (need to load before everything else) -->
-		<script type="text/javascript">
-			WebFontConfig = {
-				google: { families: [ 'Raleway:300,400,700', 'Enriqueta:400,700', 'Inconsolata:400'] }
-			};
-			(function() {
-				var wf = document.createElement('script');
-				wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-				wf.type = 'text/javascript';
-				wf.async = 'true';
-				var s = document.getElementsByTagName('script')[0];
-				s.parentNode.insertBefore(wf, s);
-			})();
-		</script>
-	    <style type="text/css">
-/***
-* Google/Typekit Webfont Loader styles
-***/
-		
-.wf-loading h1,
-.wf-loading h2,
-.wf-loading h3,
-.wf-loading h4,
-.wf-loading h5,
-.wf-loading h6 {
-font-family: sans-serif
-}
-.wf-inactive h1,
-.wf-inactive h2,
-.wf-inactive h3,
-.wf-inactive h4,
-.wf-inactive h5,
-.wf-inactive h6 {
-font-family: sans-serif
-}
-.wf-active h1,
-.wf-active h2,
-.wf-active h3,
-.wf-active h4,
-.wf-active h5,
-.wf-active h6 {
-font-family: "Raleway", "Helvetica Neue", Helvetica, Arial, sans-serif;
-}
-.wf-loading body {
-font-family: serif;
-}
-.wf-inactive body {
-font-family: serif;
-}
-.wf-active body {
-font-family: "Enriqueta", Georgia, "Times New Roman", Times, serif;
-}
-		</style>
-		
 				
 		<!-- media-queries.js (fallback) -->
 		<!--[if lt IE 9]>
@@ -101,14 +46,8 @@ font-family: "Enriqueta", Georgia, "Times New Roman", Times, serif;
 		
   		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<!-- wordpress head functions -->
-		<?php wp_head(); ?>
-		<!-- end of wordpress head -->
-
-		<!-- theme options from options panel -->
-		<?php get_wpbs_theme_options(); ?>
-				
-	</head>
+		<!-- WordPress head functions -->
+	<?php wp_head(); ?></head>
 	
 	<?php
 	$nav_position = $options['nav_position'];
@@ -152,7 +91,7 @@ font-family: "Enriqueta", Georgia, "Times New Roman", Times, serif;
 								<?php $nav_align = $options['nav_alignment']; ?>
 								<?php if ($nav_align=='left') : ?>
 									<div class="nav-collapse pull-left">
-										<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+										<?php bones_main_nav(); ?>
 									</div>
 								<?php endif; ?>
 
@@ -164,7 +103,7 @@ font-family: "Enriqueta", Georgia, "Times New Roman", Times, serif;
 									
 								<?php if ($nav_align=='right') : ?>
 									<div class="nav-collapse pull-right">
-										<?php bones_main_nav(); // Adjust using Menus in Wordpress Admin ?>
+										<?php bones_main_nav(); ?>
 									</div>
 								<?php endif; ?>
 								
