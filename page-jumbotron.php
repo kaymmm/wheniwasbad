444bc450f4885c6f81d8b898d4515c8fda76cbd2
Keith Miyake <kmiyake@gc.cuny.edu>
2013-10-15 11:32:23 -0400

    apply mods to jumbotron template from homepage template

--- page-jumbotron.php
+++ page-jumbotron.php
@@ -16,6 +16,9 @@ Template Name: Full-width Jumbotron
 	$sidebar_position = get_post_meta($post->ID, 'sidebar_position' , true);
 	$sidebar_widget_group = get_post_meta($post->ID, 'sidebar_widgets' , true);
 	$hide_empty_sidebar = $wheniwasbad_options['hide_widgets'];
+	$jumbotron_contents = get_post_meta($post->ID, 'jumbotron_contents' , true);
+	$jumbotron_bg_color = get_post_meta($post->ID, 'jumbotron_bg_color' , true);
+	$jumbotron_bg_image = get_post_meta($post->ID, 'jumbotron_bg_image' , true);
 	if ( is_active_sidebar($sidebar_widget_group) && ! $hide_empty_sidebar ) {
 		if ( $sidebar_position == 'left' ) {
 			$main_class = "col-md-9 col-md-push-3";
@@ -32,15 +35,35 @@ Template Name: Full-width Jumbotron
 
 <div id="content" class="content-no-margin clearfix">
 			
-	<header>
+	<?php if ($jumbotron_contents != ''): ?>
+
+		<?php
+			if ($jumbotron_bg_image) {
+				$jumbotron_style = 'style="background-image: url(\'' . wp_get_attachment_url($jumbotron_bg_image) . '\'); background-repeat: no-repeat; background-position: 0 0; background-size: cover;\'"';
+			} elseif ($jumbotron_bg_color) {
+				$jumbotron_style = 'style="background-color: ' . $jumbotron_bg_color . '"';
+			}
+		?>
 	
-		<div class="jumbotron" style="background-image: url('<?php echo $featured_src[0]; ?>'); background-repeat: no-repeat; background-position: 0 0;">
-		
-			<?php echo get_post_meta($post->ID, 'jumbotron_contents' , true);?>
+		<div class="jumbotron" <?php echo $jumbotron_style; ?>>
 	
+			<?php echo $jumbotron_contents;?>
+
 		</div>
 
-	</header>
+	<?php else: ?>
+		<div class="container clearfix">
+			<header class="page-header">
+			
+				<h1><?php single_post_title(); ?></h1>
+			
+				<?php get_template_part('postmeta-horizontal'); ?>
+		
+			</header> <!-- end page header -->
+
+		</div>
+
+	<?php endif;?>
 	
 	<div class="container">
 
