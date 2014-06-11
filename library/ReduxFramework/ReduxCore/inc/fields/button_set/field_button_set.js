ea9cca4afea0cf579c37e01858837ecf655f91d0
Keith Miyake <keith.miyake@gmail.com>
2014-06-11 12:23:11 -0400

    update reduxframework to master

--- library/ReduxFramework/ReduxCore/inc/fields/button_set/field_button_set.js
+++ library/ReduxFramework/ReduxCore/inc/fields/button_set/field_button_set.js
@@ -1,7 +1,50 @@
-/*global jQuery, document*/
+/*
+ Field Button Set (button_set)
+ */
 
-jQuery(document).ready(function () {
+/*global jQuery, document, redux*/
 
-  jQuery('.buttonset').buttonset();
-  
-});
+(function( $ ) {
+    "use strict";
+
+    redux.field_objects = redux.field_objects || {};
+    redux.field_objects.button_set = redux.field_objects.button_set || {};
+
+    $( document ).ready(
+        function() {
+            //redux.field_objects.button_set.init();
+        }
+    );
+
+    redux.field_objects.button_set.init = function( selector ) {
+        if ( !selector ) {
+            selector = $( document ).find( '.redux-container-button_set' );
+        }
+
+        $( selector ).each(
+            function() {
+                var el = $( this );
+                var parent = el;
+                if ( !el.hasClass( 'redux-field-container' ) ) {
+                    parent = el.parents( '.redux-field-container:first' );
+                }
+                if ( parent.hasClass( 'redux-field-init' ) ) {
+                    parent.removeClass( 'redux-field-init' );
+                } else {
+                    return;
+                }
+                el.find( '.buttonset' ).each(
+                    function() {
+                        if ( $( this ).is( ':checkbox' ) ) {
+                            $( this ).find( '.buttonset-item' ).button();
+                        }
+
+                        $( this ).buttonset();
+                    }
+                );
+            }
+        );
+
+
+    };
+})( jQuery );
